f1e0996f85ed029bb600fbf73ee0044f894c0a3a
Andrew Pecha <andrew.pecha@gmail.com>
2018-04-03 21:11:07 -0500

    Add project files.

---
+++ NinjectTest/Models/Entities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace NinjectTest.Models
+{
+    public class Entities : DbContext
+    {
+        public Entities() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+
+}
---
+++ NinjectTest/Ninject/Bindings.cs
@@ -0,0 +1,16 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NinjectTest.Ninject
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
